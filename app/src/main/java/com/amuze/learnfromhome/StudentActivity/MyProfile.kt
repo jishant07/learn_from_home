15250fb4a65009b44e51f06c27ebb2e5f0e2b738
Afsar7977 <ansariafsar@122gmail.com>
2020-09-28 17:09:12 +0530

    Account Update Part Added in Mobile Android Project.

--- app/src/main/java/com/amuze/learnfromhome/StudentActivity/MyProfile.kt
+++ app/src/main/java/com/amuze/learnfromhome/StudentActivity/MyProfile.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
